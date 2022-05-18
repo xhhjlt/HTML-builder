e37ccea5de33fd0b509331a773a36aee09d8a237
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-05-18 14:33:34 +0500

    feat: add merge-styles task solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs/promises');
+
+const stylesPath = path.resolve(__dirname, 'styles');
+const bundlePath = path.resolve(__dirname, 'project-dist', 'bundle.css');
+
+fs.writeFile(bundlePath, '');
+
+fs.readdir(stylesPath, {withFileTypes: true}).then(fileDirents => {
+  for (let i = 0; i < fileDirents.length; i++) {
+    let filePath = path.resolve(stylesPath, fileDirents[i].name);
+    let fileExt = path.extname(filePath);
+    if (fileDirents[i].isFile() && fileExt === '.css') {
+      fs.readFile(filePath, 'utf-8').then(data => fs.appendFile(bundlePath, `${data}\n`) , err => console.log(err));
+    }}
+}, err => console.log(err));
+
+
+
