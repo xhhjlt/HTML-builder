d89134d0fffcaebee665a75e0402e02bb62b2dd4
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-05-18 22:51:02 +0500

    feat: full build-page task solution

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,8 +3,11 @@ const fs = require('fs/promises');
 
 const templateFile = path.resolve(__dirname, 'template.html');
 const componentsDir = path.resolve(__dirname, 'components');
+const assetsDir = path.resolve(__dirname, 'assets');
+const stylesDir = path.resolve(__dirname, 'styles');
 const distDir = path.resolve(__dirname, 'project-dist');
 const distHTML = path.resolve(distDir, 'index.html');
+const distCSS = path.resolve(distDir, 'style.css');
 
 let template = fs.readFile(templateFile, 'utf-8');
 let components = {};
@@ -24,10 +27,39 @@ const resolveTemplate = (str) => {
   }
 };
 
+function copyDir(basePath, copyPath) {
+  const file = fs.stat(basePath);
+  file.then(file =>{
+    if (file.isDirectory()) {
+      const files = fs.readdir(basePath);
+      files.then(files => {
+        for (let i = 0; i < files.length; i++) {
+          fs.mkdir(copyPath, {recursive: true}).finally(() => copyDir(path.resolve(basePath, files[i]), path.resolve(copyPath, files[i])));
+        }
+      }, err => console.log(err));
+    } else {
+      fs.writeFile(copyPath, '').finally(() => fs.copyFile(basePath, copyPath));
+    }
+  }, err => console.log(err));
+}
+
+const bundleStyles = (stylesPath, bundlePath) => {
+  fs.readdir(stylesPath, {withFileTypes: true}).then(fileDirents => {
+    for (let i = 0; i < fileDirents.length; i++) {
+      let filePath = path.resolve(stylesPath, fileDirents[i].name);
+      let fileExt = path.extname(filePath);
+      if (fileDirents[i].isFile() && fileExt === '.css') {
+        fs.readFile(filePath, 'utf-8').then(data => fs.appendFile(bundlePath, `${data}\n`) , err => console.log(err));
+      }}
+  }, err => console.log(err));
+};
+
 fs.rm(distDir, { recursive: true, force: true }).finally(() => 
   fs.mkdir(distDir, {recursive: true}).then(() => {
     fs.writeFile(distHTML, '').then(() => {
       template.then((data) => {resolveTemplate(data);}, err => console.log(err));
     }, err => console.log(err));
+    copyDir(assetsDir, path.resolve(distDir, 'assets'));
+    fs.writeFile(distCSS, '').then(() => bundleStyles(stylesDir, distCSS));
   }, err => console.log(err))
 );
